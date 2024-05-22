5d7207eb1eed0a7a9747d21ffd325aef58fe02df
jake <jakechiu@pklotcorp.com>
2024-05-22 11:30:50 +0800

    fix:test

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig, devices } from '@playwright/test';
+import generateCustomLayoutSimpleExample from "./my_custom_layout";
+import { LogLevel } from '@slack/web-api';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 0 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: process.env.CI ? 
   [
@@ -27,10 +32,10 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
     {
       name: 'webkit',
